a07c3ebe94121ae621bfe6fdd2821ae967727f13
mys12 <mys62811@naver.com>
2019-07-17 14:43:10 +0900

    contract 1

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -4,12 +4,15 @@ using namespace eosio;
 
 CONTRACT helloworld: public contract {
     public: 
+        
         using contract::contract;
         ACTION hi(name user) {
-            check(has_auth(user), "missing authority"); // has_auth() -> true or false 
+            check(has_auth("moonmoon1234"_n), "missing authority"); // has_auth() -> true or false 
             
             // == require_auth(user);
-            
+            // require_auth("moonmoon1234"_n);
+            // require_auth(get_self()); -> 동적으로,, 할당해서 제일 추천
+            // check(has_auth(get_self(), "missing authority"));
             
             print("hello, ", user);
         }
