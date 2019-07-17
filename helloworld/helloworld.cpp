6b1bf3eb9da2e3ba4d7302060f2a55cc2322a975
mys12 <mys62811@naver.com>
2019-07-17 16:30:38 +0900

    contract getaccount

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -6,10 +6,10 @@ CONTRACT helloworld: public contract {
     public: 
         
         using contract::contract;
-        ACTION hi(name user) {
+            ACTION hi(name user) {
             // bool isTrue = has_auth(get_self()) || has_auth("hahahoho1234"_n);
             // check(isTrue, "you aint a contract deployer!!")
-            check(has_auth(get_self()) || has_auth("hahahoho1234"_n),"missing authority");
+            // check(has_auth(get_self()) || has_auth("hahahoho1234"_n),"missing authority");
             //check(has_auth("moonmoon1234"_n), "missing authority"); // has_auth() -> true or false 
             
             // == require_auth(user);
@@ -17,7 +17,11 @@ CONTRACT helloworld: public contract {
             // require_auth(get_self()); -> 동적으로,, 할당해서 제일 추천
             // check(has_auth(get_self(), "missing authority"));
             
-            print("hello, ", user);
+                print("hello, ", user);
+            }
+        ACTION getaccount(name user){
+            require_auth(get_self());
+            print(is_account(user));
         }
     private :
 
