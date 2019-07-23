9edbb0893bfd1bc6bd9fcd6b700317ea385266b7
mys12 <mys62811@naver.com>
2019-07-23 11:29:32 +0900

    available primary key2

--- available/available.cpp
+++ available/available.cpp
@@ -15,28 +15,25 @@ CONTRACT available: public contract {
             
             if(from == get_self()) {
                 Arecver receiver(get_self(), get_self().value);
-                auto itr = receiver.find(to.value);
                 
-                if(itr == receiver.end()) {
-                    receiver.emplace(from, [&](auto& row) {
-                        row.user = to;
-                        row.balance = quantity;
-                        row.mykey = receiver.available_primary_key();
-                    });
-                }
+                receiver.emplace(from, [&](auto& row) {
+                    row.user = to;
+                    row.balance = quantity;
+                    row.mykey = receiver.available_primary_key();
+                });
+
             } else {
                 Asender sender(get_self(), get_self().value);
-                auto itr = sender.find(from.value);
                 
-                if(itr == sender.end()) {
-                    sender.emplace(to, [&](auto& row) {
-                        row.user = from;
-                        row.balance = quantity;
-                        row.mykey = sender.available_primary_key();
-                    });
-                }
+                
+                sender.emplace(to, [&](auto& row) {
+                    row.user = from;
+                    row.balance = quantity;
+                    row.mykey = sender.available_primary_key();
+                });
             }
         }
+        
 
     private:
         TABLE available_struct {
@@ -50,3 +47,47 @@ CONTRACT available: public contract {
         typedef multi_index<"avasend"_n, available_struct> Asender;
         typedef multi_index<"avarecv"_n, available_struct> Arecver;
 };
+
+
+// 정답 
+// #include <eosio/eosio.hpp>
+// #include <eosio/asset.hpp>
+
+// using namespace eosio;
+
+// CONTRACT tradingbook: public contract {
+//     public:
+//         using contract::contract;
+
+//         ACTION dummy() {}
+
+//         [[eosio::on_notify("eosio.token::transfer")]]
+//         void ontransfer(name from, name to, asset quantity, std::string memo) {
+//             if(from == get_self()) {
+//                 Receive forReceiver(get_self(), get_self().value);
+//                 forReceiver.emplace(get_self(), [&](auto& row) {
+//                     row.mykey = forReceiver.available_primary_key();
+//                     row.user = from;
+//                     row.balance = quantity;
+//                 });
+//             } else {
+//                 Send forSender(get_self(), get_self().value);
+//                 forSender.emplace(get_self(), [&](auto& row) {
+//                     row.mykey = forSender.available_primary_key();
+//                     row.user = to;
+//                     row.balance = quantity;
+//                 });
+//             }
+//         }
+//     private:
+//         TABLE tradingbook_struct {
+//             uint64_t mykey;
+//             name user;
+//             asset balance;
+
+//             uint64_t primary_key() const { return mykey; }
+//         };
+
+//         typedef multi_index<"senderbook"_n, tradingbook_struct> Send;
+//         typedef multi_index<"receiverbook"_n, tradingbook_struct> Receive;
+// };
